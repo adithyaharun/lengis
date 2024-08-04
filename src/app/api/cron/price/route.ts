7635a9c7f2e28d2fa4e8f1e09e8714bfe2b09e53
Adithya <adithyaharun@outlook.com>
2024-08-04 10:35:37 +0800

    fix(cron): set to force-dynamic

--- src/app/api/cron/price/route.ts
+++ src/app/api/cron/price/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { fetchPrice } from "~/server/actions/price";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   await fetchPrice();
 
